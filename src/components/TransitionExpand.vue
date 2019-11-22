f44fd81f9433fb2e8586752581e2e8ba1a9e0edd
Amanda Baret <amanda.baret@absa.africa>
2019-11-22 11:46:05 +0200

    transition component

---
+++ src/components/TransitionExpand.vue
@@ -0,0 +1,89 @@
+
+<template>
+  <transition
+    name="expand"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @leave="leave"
+    @after-leave="afterLeave"
+  >
+    <slot/>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'TransitionExpand',
+  methods: {
+    enter(element) {
+      const width = getComputedStyle(element).width;
+
+      element.style.width = width;
+      element.style.position = 'absolute';
+      element.style.visibility = 'hidden';
+      element.style.height = 'auto';
+
+      const height = getComputedStyle(element).height;
+
+      element.style.width = null;
+      element.style.position = null;
+      element.style.visibility = null;
+      element.style.height = 0;
+
+      // Force repaint to make sure the
+      // animation is triggered correctly.
+      getComputedStyle(element).height;
+
+      // Trigger the animation.
+      // We use `setTimeout` because we need
+      // to make sure the browser has finished
+      // painting after setting the `height`
+      // to `0` in the line above.
+      setTimeout(() => {
+        element.style.height = height;
+      });
+    },
+
+    afterEnter(element) {
+      element.style.height = 'auto';
+      element.style.opacity = 1
+    },
+
+    leave(element) {
+      const height = getComputedStyle(element).height;
+      
+      element.style.height = height;
+     
+      // Force repaint to make sure the
+      // animation is triggered correctly.
+      getComputedStyle(element).height;
+
+      setTimeout(() => {
+        element.style.height = 0;
+     });
+    },
+
+    afterLeave(element) {
+      element.style.opacity = 1
+    },
+  },
+};
+
+</script>
+
+<style> 
+
+.expand-enter-active,
+.expand-leave-active {
+  transition: opacity, height 1s ease-in-out;
+  overflow: hidden;
+}
+
+.expand-enter,
+.expand-leave-to {
+  height: 0;
+  opacity: 0;
+}
+</style> 
+
+
