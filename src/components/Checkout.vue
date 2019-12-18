393fedc054511696d0bd512fc0077a9b47dc28fa
Amanda Baret <amanda.baret@absa.africa>
2019-12-18 12:31:27 +0200

    added date to orders

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -139,7 +139,7 @@ export default {
      getNow: function() {
                     const today = new Date();
                     const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-                    const time = today.getHours() + ":" + today.getMinutes() ;
+                    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
                     const dateTime = date +' '+ time;
                    return dateTime;
       },
