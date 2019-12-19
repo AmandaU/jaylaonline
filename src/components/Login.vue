36367f85a1ca16cd966c940e9855f8cebd188355
AmandaU <ursling@gmail.com>
2019-12-19 13:45:27 +0200

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,14 +2,22 @@
   <div>
      <div class="container" >
        <!-- <cube-spin v-if="busy"></cube-spin> -->
-        <div class="infoblock">
+        <div v-if="!showForgotPassword" class="infoblock">
           <h2>{{signinmessage}}</h2>
           <input type="text" v-model="email" placeholder="Email"  class="infoblockitem"><br>
           <input type="password" v-model="password" placeholder="Password"  class="infoblockitem"><br>
           <button @click="login" class="loginbutton">Login</button>
           <p>You don't have an account ? You can 
             <span @click="goToSignup()" style="color:blue;cursor:pointer">create one</span>
+           </p>
+           <p>Forgot your password? 
+            <span @click="showForgotPassword = true" style="color:blue;cursor:pointer">Send a reset email</span>
           </p>
+        </div>
+         <div v-if="showForgotPassword" class="infoblock">
+          <h2>Send recovery email to:</h2>
+          <input type="text" v-model="forgotEmail" placeholder="Recovery email"  class="infoblockitem"><br>
+           <button @click="forgotPassword" class="loginbutton">Send recovery email</button>
         </div>
      </div>
   </div>
@@ -25,6 +33,8 @@
 
   data() {
       return {
+        showForgotPassword: false,
+        forgotEmail: "",
         busy: true,
         email: "",
         password: "",
@@ -45,6 +55,20 @@
   },
  
   methods: {
+
+    forgotPassword() {
+        var auth = firebase.auth();
+        var emailAddress = "user@example.com";
+        let self = this
+        auth.sendPasswordResetEmail(this.forgotEmail).then(function() {
+                  
+            alert('Email sent successfully. Check your email for steps to reset your password' )
+              self.$router.replace({ name: 'Home'});
+           
+             }).catch(function(error) {
+            alert('We could not send the email. Please check your email address and internet connectivity');
+        });
+    },
 
     goToSignup ()
     {
